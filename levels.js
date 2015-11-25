0278134e8b2d0c916278dc22851cfadb6b158818
Thomas Park <thomas@thomaspark.co>
2015-11-24 20:52:30 -0500

    fix typo on level 1

--- levels.js
+++ levels.js
@@ -1,7 +1,7 @@
 var levels = [
   {
     name: 'justify-content 1',
-    instructions: 'Welcome to Flexbox Froggy, a game where you help Froggy and friends by writing CSS code! Guide this frog get to the lilypad on the right by using the <code>justify-content</code> property, which aligns items horizontally and accepts the following values:<ul><li><code>flex-start</code>: Items align to the left side of the container.</li><li><code>flex-end</code>: Items align to the right side of the container.</li><li><code>center</code>: Items align at the center of the container.</li><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul><p>For example, <code>justify-content: center;</code> will center the frog.</p>',
+    instructions: 'Welcome to Flexbox Froggy, a game where you help Froggy and friends by writing CSS code! Guide this frog to the lilypad on the right by using the <code>justify-content</code> property, which aligns items horizontally and accepts the following values:<ul><li><code>flex-start</code>: Items align to the left side of the container.</li><li><code>flex-end</code>: Items align to the right side of the container.</li><li><code>center</code>: Items align at the center of the container.</li><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul><p>For example, <code>justify-content: center;</code> will center the frog.</p>',
     board: 'g',
     style: {'justify-content': 'flex-end'},
     before: "#pond {\n  display: flex;\n",
