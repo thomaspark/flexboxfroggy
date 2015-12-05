12b9a2fea8731813f6038b0626b57766e89f4385
Marcus Tenório <marcusvtenorio@gmail.com>
2015-12-05 11:59:54 -0200

    Add translations in pt-br

--- js/messages.js
+++ js/messages.js
@@ -1,10 +1,12 @@
 var messages = {
   warningReset: {
     'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
-    'fr': 'Êtes-vous certain de vouloir réinitialiser la partie?\n\nVotre progrès sauvegardé sera perdu et vous recommencerez au début du jeu.'
+    'fr': 'Êtes-vous certain de vouloir réinitialiser la partie?\n\nVotre progrès sauvegardé sera perdu et vous recommencerez au début du jeu.',
+    'pt-br': 'Você tem certeza que quer reinicializar o jogo?\n\nSeu progresso salvo será perdido e você voltará ao começo do jogo.'
   },
   labelReset: {
     'en': 'Reset',
-    'fr': 'Réinitialiser'
+    'fr': 'Réinitialiser',
+    'pt-br': 'Reinicializar'
   }
 };
