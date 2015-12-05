a841f128c123677e05b6333634149a10193eef2f
tfrommen <info@tfrommen.de>
2015-12-05 15:42:16 +0100

    Translate new strings.

--- js/messages.js
+++ js/messages.js
@@ -1,10 +1,12 @@
 var messages = {
   warningReset: {
     'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
+    'de': 'Bist su sicher, dass du das Spiel zur&uuml;cksetzen m&ouml;chtest?\n\nDein gespeicherter Fortschritt geht dabei verloren und du musst das Spiel von neuem starten.',
     'fr': 'Êtes-vous certain de vouloir réinitialiser la partie?\n\nVotre progrès sauvegardé sera perdu et vous recommencerez au début du jeu.'
   },
   labelReset: {
     'en': 'Reset',
+    'de': 'Zur&uuml;cksetzen',
     'fr': 'Réinitialiser'
   }
 };
