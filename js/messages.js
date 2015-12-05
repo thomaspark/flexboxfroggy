ef16c4c06d6ea7c98ee7346fd678171dfc47d53d
manuelitox <Manuel2788@gmail.com>
2015-12-05 19:21:08 -0430

    Adding spanish translation in the messages.js file

--- js/messages.js
+++ js/messages.js
@@ -3,12 +3,14 @@ var messages = {
     'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
     'de': 'Bist su sicher, dass du das Spiel zurücksetzen möchtest?\n\nDein gespeicherter Fortschritt geht dabei verloren und du musst das Spiel von neuem starten.',
     'fr': 'Êtes-vous certain de vouloir réinitialiser la partie?\n\nVotre progrès sauvegardé sera perdu et vous recommencerez au début du jeu.',
-    'pt-br': 'Você tem certeza que quer reinicializar o jogo?\n\nSeu progresso salvo será perdido e você voltará ao começo do jogo.'
+    'pt-br': 'Você tem certeza que quer reinicializar o jogo?\n\nSeu progresso salvo será perdido e você voltará ao começo do jogo.',
+    'es': 'Estás seguro, ¿Quieres reiniciar el juego?\n\nTu progreso se perderá y serás enviado al inicio del juego.'
   },
   labelReset: {
     'en': 'Reset',
     'de': 'Zurücksetzen',
     'fr': 'Réinitialiser',
-    'pt-br': 'Reinicializar'
+    'pt-br': 'Reinicializar',
+    'es': 'Reiniciar'
   }
 };
