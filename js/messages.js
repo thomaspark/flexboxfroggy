9860f2a034407baefa67f4c3550a2eb5566ae754
Artem Vozhzhov <vojjov.artem@ya.ru>
2015-12-07 13:09:44 +0800

    Added russian messages and modified some strings in index file

--- js/messages.js
+++ js/messages.js
@@ -4,13 +4,15 @@ var messages = {
     'de': 'Bist su sicher, dass du das Spiel zurücksetzen möchtest?\n\nDein gespeicherter Fortschritt geht dabei verloren und du musst das Spiel von neuem starten.',
     'fr': 'Êtes-vous certain de vouloir réinitialiser la partie?\n\nVotre progrès sauvegardé sera perdu et vous recommencerez au début du jeu.',
     'pt-br': 'Você tem certeza que quer reinicializar o jogo?\n\nSeu progresso salvo será perdido e você voltará ao começo do jogo.',
-    'es': 'Estás seguro, ¿Quieres reiniciar el juego?\n\nTu progreso se perderá y serás enviado al inicio del juego.'
+    'es': 'Estás seguro, ¿Quieres reiniciar el juego?\n\nTu progreso se perderá y serás enviado al inicio del juego.',
+    'ru': 'Вы действительно хотите сбросить игру?\n\nВаш прогресс будет потерян и вы вернетесь к началу игры.'
   },
   labelReset: {
     'en': 'Reset',
     'de': 'Zurücksetzen',
     'fr': 'Réinitialiser',
     'pt-br': 'Reinicializar',
-    'es': 'Reiniciar'
+    'es': 'Reiniciar',
+    'ru': 'Сбросить'
   }
 };
