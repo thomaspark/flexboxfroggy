bae545927c033c406dd3ef019e1d7cac3d5db0f0
Uğur Korfalı <ugurkorfali@gmail.com>
2015-12-10 08:16:52 +0200

    Update messages.js
    tr translations have been added

--- js/messages.js
+++ js/messages.js
@@ -6,7 +6,8 @@ var messages = {
     'pt-br': 'Próximo',
     'es': 'Siguiente',
     'ru': 'Следующий',
-    'fa': 'مرحله بعدی'
+    'fa': 'مرحله بعدی',
+    'tr': 'Sonraki'
   },
   labelFooter: {
     'en': 'Flexbox Froggy is created by',
@@ -16,6 +17,7 @@ var messages = {
     'es': 'Flexbox Froggy es una creación de',
     'ru': 'Игра Flexbox Froggy создана',
     'fa': 'قورباغه فلکس باکس ساخته شده توسط',
+    'tr': 'Flexbox Froggy\'nin yaratıcısı:'
   },
   labelLevel: {
     'en': 'Level',
@@ -24,7 +26,8 @@ var messages = {
     'pt-br': 'Nível',
     'es': 'Nivel',
     'ru': 'Уровень',
-    'fa': 'مرحله'
+    'fa': 'مرحله',
+    'tr': 'Seviye',
   },
   labelOf: {
     'en': 'of',
@@ -33,7 +36,8 @@ var messages = {
     'pt-br': 'de',
     'es': 'de',
     'ru': 'из',
-    'fa': 'از'
+    'fa': 'از',
+    'tr': '/'
   },
   title: {
     'en': 'Flexbox Froggy - A game for learning CSS flexbox',
@@ -42,7 +46,8 @@ var messages = {
     'pt-br': 'Flexbox Froggy - Um jogo para aprender CSS flexbox',
     'es': 'Flexbox Froggy - Un juego para aprender CSS flexbox',
     'ru': 'Flexbox Froggy - Игра для изучения CSS Flexbox',
-    'fa': 'قورباغه فلکس باکس - یک باز برای آموزش سی اس اس فبکس باکس'
+    'fa': 'قورباغه فلکس باکس - یک باز برای آموزش سی اس اس فبکس باکس',
+    'tr': 'Flexbox Froggy - CSS flexbox için öğretici oyun'
   },
   labelReset: {
     'en': 'Reset',
@@ -51,7 +56,8 @@ var messages = {
     'pt-br': 'Reinicializar',
     'es': 'Reiniciar',
     'ru': 'Сбросить',
-    'fa': 'شروع مجدد'
+    'fa': 'شروع مجدد',
+    'tr': 'Baştan Başla'
   },
   warningReset: {
     'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
@@ -60,6 +66,7 @@ var messages = {
     'pt-br': 'Você tem certeza que quer reinicializar o jogo?\n\nSeu progresso salvo será perdido e você voltará ao começo do jogo.',
     'es': 'Estás seguro, ¿Quieres reiniciar el juego?\n\nTu progreso se perderá y serás enviado al inicio del juego.',
     'ru': 'Вы действительно хотите сбросить игру?\n\nВаш прогресс будет потерян и вы вернетесь к началу игры.',
-    'fa': 'آیا از شروع مجدد بازی اطمینان دارید ?\n\nتمام مراحل ذخیره شده حذف خواهند شد.'
+    'fa': 'آیا از شروع مجدد بازی اطمینان دارید ?\n\nتمام مراحل ذخیره شده حذف خواهند شد.',
+    'tr': 'Oyuna baştan başlamak istediğinize emin misiniz? Kayıtlı bütün ilerlemeniz silinecek ve birinci seviyeye geri döneceksiniz.'
   }
 };
