6b389c239f32d8625c28e462f3617e310cadbcce
liberaliscomputing <liberalis.computing@gmail.com>
2017-04-04 17:47:19 -0400

    modify index.html and game.js to collect user logs in Parse

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
-var firebase = new Firebase('https://blinding-heat-5896.firebaseio.com/web/data');
-var analytics = firebase.child('analytics');
+Parse.initialize("FwVMmzHookZZ5j9F9ILc2E5MT5ufabuV7hCXKSeu");
+Parse.serverURL = 'http://129.25.12.218:1337/parse';
+var Submission = Parse.Object.extend("FlexboxFroggy");
+
 var game = {
   language: window.location.hash.substring(1) || 'en',
   level: parseInt(localStorage.level, 10) || 0,
@@ -305,6 +307,8 @@ var game = {
       }
     });
 
+    var submission = new Submission();
+
     if (correct) {
       ga('send', {
         hitType: 'event',
@@ -313,7 +317,7 @@ var game = {
         eventLabel: $('#code').val()
       });
 
-      analytics.push({
+      submission.save({
         timeStamp: (new Date()).getTime(),
         user: game.user,
         levelName: level.name,
@@ -336,7 +340,7 @@ var game = {
         eventLabel: $('#code').val()
       });
       
-      analytics.push({
+      submission.save({
         timeStamp: (new Date()).getTime(),
         user: game.user,
         levelName: level.name,
